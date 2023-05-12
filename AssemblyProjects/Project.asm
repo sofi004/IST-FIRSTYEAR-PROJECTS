0abf34952d9db2c69a39302c420f6a4fd524c70f
ist1106194 <sophiapiteira@gmail.com>
2023-05-12 13:50:14 +0100

    image media center

--- AssemblyProjects/Project.asm
+++ AssemblyProjects/Project.asm
@@ -6,7 +6,14 @@
 ; ******************************************************************************
 ; * Constantes
 ; ******************************************************************************
+COMANDOS				EQU	6000H			; endereço de base dos comandos do MediaCenter
 
+DEFINE_LINHA    		EQU COMANDOS + 0AH		; endereço do comando para definir a linha
+DEFINE_COLUNA   		EQU COMANDOS + 0CH		; endereço do comando para definir a coluna
+DEFINE_PIXEL    		EQU COMANDOS + 12H		; endereço do comando para escrever um pixel
+APAGA_AVISO     		EQU COMANDOS + 40H		; endereço do comando para apagar o aviso de nenhum cenário selecionado
+APAGA_ECRÃ	 		EQU COMANDOS + 02H		; endereço do comando para apagar todos os pixels já desenhados
+SELECIONA_CENARIO_FUNDO  EQU COMANDOS + 42H		; endereço do comando para selecionar uma imagem de fundo
 ; ##############################################################################
 ; * ZONA DE DADOS 
 ; ##############################################################################
@@ -14,5 +21,9 @@
 ; ******************************************************************************
 ; * Código
 ; ******************************************************************************
-
+inicio:
+     MOV  [APAGA_AVISO], R1	; apaga o aviso de nenhum cenário selecionado (o valor de R1 não é relevante)
+     MOV  [APAGA_ECRÃ], R1	; apaga todos os pixels já desenhados (o valor de R1 não é relevante)
+	MOV	R1, 0			; cenário de fundo número 0
+     MOV  [SELECIONA_CENARIO_FUNDO], R1	; seleciona o cenário de fundo
 ;###############################################################################
